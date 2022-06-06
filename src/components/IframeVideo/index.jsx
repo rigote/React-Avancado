cc8bdd1b96bc8cfb61a0d4b7d25a4ec2a36d09ef
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-06-06 17:04:26 -0300

    Fix chat

--- src/components/IframeVideo/index.jsx
+++ src/components/IframeVideo/index.jsx
@@ -6,11 +6,16 @@ const IframeVideo = () => {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/live_chat?v=QdgAJqF1SaU&embed_domain=https://rigote-react-avancado.vercel.app"
+        src="https://www.youtube.com/embed/QdgAJqF1SaU"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+      ></iframe>
+      <iframe
+        width="150px"
+        height="100px"
+        src="https://www.youtube.com/live_chat?v=QdgAJqF1SaU&amp;embed_domain=rigote-react-avancado.vercel.app"
       ></iframe>
     </S.Wrapper>
   )
