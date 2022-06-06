19a2dcb0997e03476e951339bd82d93a6ee79d77
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-06-06 17:09:33 -0300

    Fix

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,7 +6,7 @@ const Main = ({
   description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
   <S.Wrapper>
-    <S.Logo
+    {/* <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
@@ -15,7 +15,7 @@ const Main = ({
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
-    />
+    /> */}
     <IframeVideo />
   </S.Wrapper>
 )
