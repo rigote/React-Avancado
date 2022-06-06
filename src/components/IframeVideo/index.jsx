dcbc8a728a12a62347422f8f970a5ce364643a08
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-06-06 17:08:12 -0300

    Fix

--- src/components/IframeVideo/index.jsx
+++ src/components/IframeVideo/index.jsx
@@ -4,17 +4,8 @@ const IframeVideo = () => {
   return (
     <S.Wrapper>
       <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/QdgAJqF1SaU"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-      <iframe
-        width="150px"
-        height="100px"
+        width="400px"
+        height="565px"
         src="https://www.youtube.com/live_chat?v=QdgAJqF1SaU&amp;embed_domain=rigote-react-avancado.vercel.app"
       ></iframe>
     </S.Wrapper>
